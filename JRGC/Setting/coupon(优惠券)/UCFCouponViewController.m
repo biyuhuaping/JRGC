b88d1788d506c5816831749c428742d85066cdd9
biyuhuaping <biyuhuaping@163.com>
2016-11-20 21:20:34 +0800

    修改优惠券显示不全的bug

--- JRGC/Setting/coupon(优惠券)/UCFCouponViewController.m
+++ JRGC/Setting/coupon(优惠券)/UCFCouponViewController.m
@@ -32,10 +32,6 @@
 @property (weak, nonatomic) IBOutlet UCFSelectedView *itemSelectView;   // 选项条
 @property (assign, nonatomic) NSInteger currentSelectedState;           //当前选中状态
 
-@property (strong, nonatomic) UIImageView *warnPoint1;              //返现券红点
-@property (strong, nonatomic) UIImageView *warnPoint2;              //返息券红点
-@property (strong, nonatomic) UIImageView *warnPoint3;              //兑换券红点
-
 @property (strong, nonatomic) UCFCouponReturn   *couponReturnView;      //返现券
 @property (strong, nonatomic) UCFCouponInterest *couponInterestView;    //返息券
 //@property (strong, nonatomic) UIViewController  *currentViewController; //当前viewCotl
@@ -43,6 +39,7 @@
 @property (strong, nonatomic) NSMutableArray *listArray;
 
 @property (nonatomic, strong) MLMOptionSelectView *cellViewShowList;//弹框控件
+@property (assign, nonatomic) BOOL isFirst;
 
 @end
 
@@ -68,12 +65,48 @@
         [weakSelfList performSelector:@selector(listShowDidSelect:) withObject:idexnum afterDelay:0.3];//***下拉弹框点击后消失，在执行跳转操作
     };
     
-    [self initViewController];
-    [self initWarnPoint];
+    //返现券
+    _couponReturnView = [[UCFCouponReturn alloc]initWithNibName:@"UCFCouponReturn" bundle:nil];
+    _couponReturnView.status = @"1";
+    _couponReturnView.view.frame = CGRectMake(0, 44, ScreenWidth, ScreenHeight - 108);
+    [self addChildViewController:_couponReturnView];
+    
+    //返息券
+    _couponInterestView = [[UCFCouponInterest alloc]initWithNibName:@"UCFCouponInterest" bundle:nil];
+    _couponInterestView.status = @"1";
+    _couponInterestView.view.frame = CGRectMake(0, 44, ScreenWidth, ScreenHeight - 108);
+    [self addChildViewController:_couponInterestView];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [self.navigationController setNavigationBarHidden:NO animated:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    if (_isFirst) {
+        return;
+    }
+    _isFirst = YES;
+    
+    switch (_segmentIndex) {
+        default: {//返现券
+            // 需要显示的子ViewController，要将其View添加到父View中
+            [self.view addSubview:_couponReturnView.view];
+            _currentViewController = _couponReturnView;
+        }
+            break;
+        case 1: {//返息券
+            // 需要显示的子ViewController，要将其View添加到父View中
+            [self.view addSubview:_couponInterestView.view];
+            _currentViewController = _couponInterestView;
+        }
+            break;
+    }
+    _itemSelectView.segmentedControl.selectedSegmentIndex = _segmentIndex;
+    _couponReturnView.view.frame = CGRectMake(0, 44, ScreenWidth, ScreenHeight - 108);
+    _couponInterestView.view.frame = CGRectMake(0, 44, ScreenWidth, ScreenHeight - 108);
 }
 
 - (void)addRightButtonWithName:(NSString *)rightButtonName{
@@ -90,58 +123,6 @@
     
     UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithCustomView:rightbutton];
     self.navigationItem.rightBarButtonItem = rightItem;
-}
-
-- (void)initViewController{
-    //返现券
-    _couponReturnView = [[UCFCouponReturn alloc]initWithNibName:@"UCFCouponReturn" bundle:nil];
-    _couponReturnView.status = @"1";
-    _couponReturnView.view.frame = CGRectMake(0, 44, CGRectGetWidth(super.view.bounds), CGRectGetHeight(super.view.bounds) - 44);
-    [self addChildViewController:_couponReturnView];
-    
-    //返息券
-    _couponInterestView = [[UCFCouponInterest alloc]initWithNibName:@"UCFCouponInterest" bundle:nil];
-    _couponInterestView.status = @"1";
-    _couponInterestView.view.frame = CGRectMake(0, 44, ScreenWidth, ScreenHeight - 108);
-    [self addChildViewController:_couponInterestView];
-    
-    switch (_segmentIndex) {
-        default: {//返现券
-            // 需要显示的子ViewController，要将其View添加到父View中
-            [self.view addSubview:_couponReturnView.view];
-            _currentViewController = _couponReturnView;
-        }
-            break;
-        case 1: {//返息券
-            // 需要显示的子ViewController，要将其View添加到父View中
-            [self.view addSubview:_couponInterestView.view];
-            _currentViewController = _couponInterestView;
-        }
-            break;
-    }
-}
-
-- (void)initWarnPoint{
-    CGFloat segmenWidth = ScreenWidth;
-    UIImage *image = [UIImage imageNamed:@"reward_piont"];
-    
-    _warnPoint1 = [[UIImageView alloc]initWithFrame:CGRectMake(segmenWidth/3 - 15, 0, 13, 17)];
-    _warnPoint1.image = image;
-//    [_segmentedCtrl addSubview:_warnPoint1];
-    
-    _warnPoint2 = [[UIImageView alloc]initWithFrame:CGRectMake(segmenWidth*2/3 - 15, 0, 13, 17)];
-    _warnPoint2.image = image;
-//    [_segmentedCtrl addSubview:_warnPoint2];
-    
-    _warnPoint3 = [[UIImageView alloc]initWithFrame:CGRectMake(segmenWidth - 15, 0, 13, 17)];
-    _warnPoint3.image = image;
-//    [_segmentedCtrl addSubview:_warnPoint3];
-    
-    _warnPoint1.hidden = YES;
-    _warnPoint2.hidden = YES;
-    _warnPoint3.hidden = YES;
-    
-    [self getRedPointMessage];
 }
 
 // 选项的点击事件
@@ -231,57 +212,6 @@
         }
             break;
     }
-}
-
-#pragma mark - 网络请求
-// 获取红点信息
-- (void)getRedPointMessage{
-    NSString *strParameters = [NSString stringWithFormat:@"userId=%@", [[NSUserDefaults standardUserDefaults] valueForKey:UUID]];
-    [[NetworkModule sharedNetworkModule] postReq:strParameters tag:kSXTagGetRedPointMessage owner:self];
-}
-
-//开始请求
-- (void)beginPost:(kSXTag)tag{
-//    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
-}
-
-//请求成功及结果
-- (void)endPost:(id)result tag:(NSNumber *)tag{
-    [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
-    
-    NSString *data = (NSString *)result;
-    NSMutableDictionary *dic = [data objectFromJSONString];
-    
-    // 获取红点信息
-    if (tag.intValue == kSXTagGetRedPointMessage){
-//        DBLOG(@"获取红点信息:%@",dic);
-        _warnPoint1.hidden = ![dic[@"beanRecordCount"] boolValue];//返现券红点
-        _warnPoint2.hidden = ![dic[@"interestCount"] boolValue];//返息券红点
-        _warnPoint3.hidden = ![dic[@"voucherCount"] boolValue];//兑换券红点
-        
-        switch (_currentSelectedState) {
-            case 0:{
-                _warnPoint1.hidden = YES;
-            }
-                break;
-                
-            case 1: {
-                _warnPoint2.hidden = YES;
-            }
-                break;
-                
-            case 2: {
-                _warnPoint3.hidden = YES;
-            }
-                break;
-        }
-    }
-}
-
-//请求失败
-- (void)errorPost:(NSError*)err tag:(NSNumber*)tag{
-    [MBProgressHUD displayHudError:err.userInfo[@"NSLocalizedDescription"]];
-//    [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
 }
 
 @end
