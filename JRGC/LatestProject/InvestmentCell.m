7713ead47fc896d637d3e0ed1a88a5a2a9658967
njw <niujianwei@ucfgroup.com>
2017-01-13 14:51:35 +0800

    修改 认购不能点击的问题

--- JRGC/LatestProject/InvestmentCell.m
+++ JRGC/LatestProject/InvestmentCell.m
@@ -29,7 +29,7 @@
 
 - (IBAction)investmentClick:(id)sender {
     if ([self.delegate isKindOfClass:[UCFLatestProjectViewController class]]) {
-        if (self.delegate && [self.delegate respondsToSelector:@selector(investBtnClicked:)] && [self.progressView.textStr isEqualToString:@"投资"]) {
+        if (self.delegate && [self.delegate respondsToSelector:@selector(investBtnClicked:)] && ([self.progressView.textStr isEqualToString:@"投资"] || [self.progressView.textStr isEqualToString:@"认购"])) {
             [self.delegate investBtnClicked:sender];
         }
     } else if ([self.delegate isKindOfClass:[UCFAssignmentCreditorViewController class]]){
