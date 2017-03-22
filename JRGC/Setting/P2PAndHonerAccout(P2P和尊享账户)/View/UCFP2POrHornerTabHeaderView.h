2e7e9e454044ecc2e2c12ff64eaf8e93278106a8
hanqiyuan <3152950791@qq.com>
2017-03-22 18:24:38 +0800

    添加P2P和尊享账户页面

---
+++ JRGC/Setting/P2PAndHonerAccout(P2P和尊享账户)/View/UCFP2POrHornerTabHeaderView.h
@@ -0,0 +1,26 @@
+//
+//  UCFP2POrHornerTabHeaderView.h
+//  JRGC
+//
+//  Created by hanqiyuan on 2017/3/22.
+//  Copyright © 2017年 qinwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol UCFP2POrHornerTabHeaderViewDelete <NSObject>
+
+@optional
+
+- (void)cilckShowOrHideAccoutMoney:(UIButton *)btn;
+- (void)checkP2POrHonerAccout;
+@end
+
+@interface UCFP2POrHornerTabHeaderView : UIView
+@property (strong, nonatomic) IBOutlet UILabel *accumulatedIncomeLab;//累计收益
+@property (strong, nonatomic) IBOutlet UILabel *totalIncomeLab;//累计收益
+@property (strong, nonatomic) IBOutlet UILabel *totalIncomeTitleLab;//累计收益标题
+@property (strong, nonatomic) IBOutlet UILabel *availableAmountLab;//累计收益
+;
+@property (nonatomic,assign) id<UCFP2POrHornerTabHeaderViewDelete> delegate;
+@end
