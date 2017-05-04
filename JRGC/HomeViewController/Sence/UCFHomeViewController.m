6916fd2ab1e6fcd1d568561ace06b11e2f41bdef
njw <niujianwei@ucfgroup.com>
2017-05-04 10:57:23 +0800

    新建首页类 替换UCFLatestProjectViewController

---
+++ JRGC/HomeViewController/Sence/UCFHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  UCFHomeViewController.m
+//  JRGC
+//
+//  Created by njw on 2017/5/4.
+//  Copyright © 2017年 qinwei. All rights reserved.
+//
+
+#import "UCFHomeViewController.h"
+
+@interface UCFHomeViewController ()
+
+@end
+
+@implementation UCFHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
