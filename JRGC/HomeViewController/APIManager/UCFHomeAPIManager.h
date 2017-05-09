2b7e41d9397d8abc226d541565562099c840a147
njw <niujianwei@ucfgroup.com>
2017-05-09 14:14:58 +0800

    添加首页接口

---
+++ JRGC/HomeViewController/APIManager/UCFHomeAPIManager.h
@@ -0,0 +1,15 @@
+//
+//  UCFHomeAPIManager.h
+//  JRGC
+//
+//  Created by njw on 2017/5/9.
+//  Copyright © 2017年 qinwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^NetworkCompletionHandler)(NSError *error, id result);
+
+@interface UCFHomeAPIManager : NSObject
+
+@end
---
+++ JRGC/HomeViewController/APIManager/UCFHomeAPIManager.m
@@ -0,0 +1,18 @@
+//
+//  UCFHomeAPIManager.m
+//  JRGC
+//
+//  Created by njw on 2017/5/9.
+//  Copyright © 2017年 qinwei. All rights reserved.
+//
+
+#import "UCFHomeAPIManager.h"
+#import "NetworkModule.h"
+
+@interface UCFHomeAPIManager () <NetworkModuleDelegate>
+
+@end
+
+@implementation UCFHomeAPIManager
+
+@end
