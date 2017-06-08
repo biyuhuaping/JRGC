f29ad6e02242a2dfcda7858a41f127746d65ca86
njw <niujianwei@ucfgroup.com>
2017-06-08 18:04:29 +0800

    添加 投资页面的请求类

---
+++ JRGC/Home/HomeViewController/APIManager/UCFHomeAPIManager.h
@@ -0,0 +1,23 @@
+//
+//  UCFHomeAPIManager.h
+//  JRGC
+//
+//  Created by njw on 2017/5/9.
+//  Copyright © 2017年 qinwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^NetworkCompletionHandler)(NSError *error, id result);
+
+@interface UCFHomeAPIManager : NSObject
+- (void)fetchHomeListWithUserId:(NSString *)userId completionHandler:(NetworkCompletionHandler)completionHandler;
+
+- (void)fetchUserInfoOneWithUserId:(NSString *)userId completionHandler:(NetworkCompletionHandler)completionHandler;
+
+- (void)fetchUserInfoTwoWithUserId:(NSString *)userId completionHandler:(NetworkCompletionHandler)completionHandler;
+
+- (void)fetchSignInfo:(NSString *)userId token:(NSString *)token completionHandler:(NetworkCompletionHandler)completionHandler;
+
+- (void)fetchProDetailInfoWithParameter:(NSDictionary *)parameter completionHandler:(NetworkCompletionHandler)completionHandler;
+@end
