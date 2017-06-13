d5605e32fc1bd60f642f5ba0fc51df12235bc4e4
njw <niujianwei@ucfgroup.com>
2017-06-13 18:58:59 +0800

    修改已售罄的bug

--- JRGC/Home/HomeViewController/View/UCFHomeListCell.m
+++ JRGC/Home/HomeViewController/View/UCFHomeListCell.m
@@ -11,6 +11,7 @@
 #import "NZLabel.h"
 #import "UCFProjectLabel.h"
 #import "UCFMicroMoneyModel.h"
+#import "UCFAngleView.h"
 
 @interface UCFHomeListCell ()
 @property (weak, nonatomic) IBOutlet UILabel *proName;
@@ -29,6 +30,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *repayModelLabel;
 @property (weak, nonatomic) IBOutlet UILabel *startMoneyLabel;
 @property (weak, nonatomic) IBOutlet UILabel *remainLabel;
+@property (weak, nonatomic) IBOutlet UCFAngleView *angleView;
 
 @property (weak, nonatomic) IBOutlet UIView *oneImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *oneImageBackView;
@@ -38,8 +40,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *oneImageDescribeLabel;
 @property (weak, nonatomic) IBOutlet UILabel *oneImageNumLabel;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *numBackViewW;
-@property (weak, nonatomic) IBOutlet UILabel *characteristicLabel;
-@property (weak, nonatomic) IBOutlet UIView *characteristicBackView;
+
 
 @property (weak, nonatomic) IBOutlet UIView *upSegLine;
 @property (weak, nonatomic) IBOutlet UIView *downSegLine;
@@ -130,24 +131,33 @@
             self.circleProgressView.progressText = @"认购";
         }
         else
-            self.circleProgressView.progressText = statusArr[status];
+            self.circleProgressView.progressText = @"已售罄";
         
+        self.angleView.angleStatus = presenter.item.status;
+//        DBLOG(@"%@", model.status);
         if (presenter.item.prdLabelsList.count>0) {
-            UCFProjectLabel *projectLabel = [presenter.item.prdLabelsList firstObject];
-            if ([projectLabel.labelPriority integerValue] == 1) {
-                self.characteristicBackView.hidden = NO;
-                NSDictionary *attrs = @{NSFontAttributeName : [UIFont systemFontOfSize:10]};
-                CGSize nameSize = [projectLabel.labelName boundingRectWithSize:CGSizeMake(MAXFLOAT, 12) options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
-                self.signViewWidth.constant = nameSize.width + 11;
-                self.characteristicLabel.text = projectLabel.labelName;
-            }
-            else {
-                self.characteristicBackView.hidden = YES;
+            for (UCFProjectLabel *projectLabel in presenter.item.prdLabelsList) {
+                if ([projectLabel.labelPriority integerValue] == 1) {
+                    self.angleView.angleString = [NSString stringWithFormat:@"%@", projectLabel.labelName];
+                }
             }
         }
-        else {
-            self.characteristicBackView.hidden = YES;
-        }
+//        if (presenter.item.prdLabelsList.count>0) {
+//            UCFProjectLabel *projectLabel = [presenter.item.prdLabelsList firstObject];
+//            if ([projectLabel.labelPriority integerValue] == 1) {
+//                self.characteristicBackView.hidden = NO;
+//                NSDictionary *attrs = @{NSFontAttributeName : [UIFont systemFontOfSize:10]};
+//                CGSize nameSize = [projectLabel.labelName boundingRectWithSize:CGSizeMake(MAXFLOAT, 12) options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
+//                self.signViewWidth.constant = nameSize.width + 11;
+//                self.characteristicLabel.text = projectLabel.labelName;
+//            }
+//            else {
+//                self.characteristicBackView.hidden = YES;
+//            }
+//        }
+//        else {
+//            self.characteristicBackView.hidden = YES;
+//        }
     }
     else if (presenter.modelType == UCFHomeListCellModelTypeOneImageBatchLending || presenter.modelType == UCFHomeListCellModelTypeOneImageHonorTransfer || presenter.modelType == UCFHomeListCellModelTypeOneImageBondTransfer)  {
         self.oneImageView.hidden = NO;
@@ -265,24 +275,24 @@
         self.circleProgressView.progressText = @"批量出借";
     }
     else
-        self.circleProgressView.progressText = statusArr[status];
+        self.circleProgressView.progressText = @"已售罄";
     
-    if (microMoneyModel.prdLabelsList.count>0) {
-        UCFProjectLabel *projectLabel = [microMoneyModel.prdLabelsList firstObject];
-        if ([projectLabel.labelPriority integerValue] == 1) {
-            self.characteristicBackView.hidden = NO;
-            NSDictionary *attrs = @{NSFontAttributeName : [UIFont systemFontOfSize:10]};
-            CGSize nameSize = [projectLabel.labelName boundingRectWithSize:CGSizeMake(MAXFLOAT, 12) options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
-            self.signViewWidth.constant = nameSize.width + 11;
-            self.characteristicLabel.text = projectLabel.labelName;
-        }
-        else {
-            self.characteristicBackView.hidden = YES;
-        }
-    }
-    else {
-        self.characteristicBackView.hidden = YES;
-    }
+//    if (microMoneyModel.prdLabelsList.count>0) {
+//        UCFProjectLabel *projectLabel = [microMoneyModel.prdLabelsList firstObject];
+//        if ([projectLabel.labelPriority integerValue] == 1) {
+//            self.characteristicBackView.hidden = NO;
+//            NSDictionary *attrs = @{NSFontAttributeName : [UIFont systemFontOfSize:10]};
+//            CGSize nameSize = [projectLabel.labelName boundingRectWithSize:CGSizeMake(MAXFLOAT, 12) options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
+//            self.signViewWidth.constant = nameSize.width + 11;
+//            self.characteristicLabel.text = projectLabel.labelName;
+//        }
+//        else {
+//            self.characteristicBackView.hidden = YES;
+//        }
+//    }
+//    else {
+//        self.characteristicBackView.hidden = YES;
+//    }
     if (microMoneyModel.platformSubsidyExpense.length > 0) {//贴
         self.image1W.constant = 18;
         self.proImageView1.image = [UIImage imageNamed:@"invest_icon_buletie"];
@@ -342,24 +352,30 @@
     
     NSInteger status = [microMoneyModel.status integerValue];
     NSArray *statusArr = @[@"未审核",@"等待确认",@"认购",@"流标",@"满标",@"回款中",@"已回款"];
-    self.circleProgressView.progressText = statusArr[status];
-    
-    if (microMoneyModel.prdLabelsList.count>0) {
-        UCFProjectLabel *projectLabel = [microMoneyModel.prdLabelsList firstObject];
-        if ([projectLabel.labelPriority integerValue] == 1) {
-            self.characteristicBackView.hidden = NO;
-            NSDictionary *attrs = @{NSFontAttributeName : [UIFont systemFontOfSize:10]};
-            CGSize nameSize = [projectLabel.labelName boundingRectWithSize:CGSizeMake(MAXFLOAT, 12) options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
-            self.signViewWidth.constant = nameSize.width + 11;
-            self.characteristicLabel.text = projectLabel.labelName;
-        }
-        else {
-            self.characteristicBackView.hidden = YES;
-        }
+    if (status>2) {
+        self.circleProgressView.progressText = @"已售罄";
     }
     else {
-        self.characteristicBackView.hidden = YES;
+        self.circleProgressView.progressText = statusArr[status];
     }
+    
+    
+//    if (microMoneyModel.prdLabelsList.count>0) {
+//        UCFProjectLabel *projectLabel = [microMoneyModel.prdLabelsList firstObject];
+//        if ([projectLabel.labelPriority integerValue] == 1) {
+//            self.characteristicBackView.hidden = NO;
+//            NSDictionary *attrs = @{NSFontAttributeName : [UIFont systemFontOfSize:10]};
+//            CGSize nameSize = [projectLabel.labelName boundingRectWithSize:CGSizeMake(MAXFLOAT, 12) options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
+//            self.signViewWidth.constant = nameSize.width + 11;
+//            self.characteristicLabel.text = projectLabel.labelName;
+//        }
+//        else {
+//            self.characteristicBackView.hidden = YES;
+//        }
+//    }
+//    else {
+//        self.characteristicBackView.hidden = YES;
+//    }
     if (microMoneyModel.platformSubsidyExpense.length > 0) {//贴
         self.image1W.constant = 18;
         self.proImageView1.image = [UIImage imageNamed:@"invest_icon_buletie"];
