e1c378d91f441ee51617c9cd8118a293b48909e9
njw <niujianwei@ucfgroup.com>
2017-06-16 14:56:21 +0800

    修改转让排序

--- JRGC/Invest(投资模块)/Invest(投资界面)/Controller/UCFInvestTransferViewController.m
+++ JRGC/Invest(投资模块)/Invest(投资界面)/Controller/UCFInvestTransferViewController.m
@@ -236,6 +236,10 @@
 - (void)refreshData
 {
     currentPage = 1;
+    if ([self.tableview.header isRefreshing]) {
+        [self initData];
+        [_transferHeaderView initData];
+    }
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self loadNetData];
     });
