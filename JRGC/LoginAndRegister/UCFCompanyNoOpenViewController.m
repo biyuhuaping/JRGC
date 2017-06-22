86083f0b77e9dc757eca85276b5aa71b47659f2a
hanqiyuan <3152950791@qq.com>
2017-06-22 18:23:48 +0800

    添加企业登录，以及提现提示信息修改

---
+++ JRGC/LoginAndRegister/UCFCompanyNoOpenViewController.m
@@ -0,0 +1,45 @@
+//
+//  UCFCompanyNoOpenViewController.m
+//  JRGC
+//
+//  Created by hanqiyuan on 2017/6/22.
+//  Copyright © 2017年 qinwei. All rights reserved.
+//
+
+#import "UCFCompanyNoOpenViewController.h"
+
+@interface UCFCompanyNoOpenViewController ()
+- (IBAction)clickOutLoginBtn:(id)sender;
+
+@end
+
+@implementation UCFCompanyNoOpenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)clickOutLoginBtn:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:^{
+        
+        
+    }];
+}
+@end
