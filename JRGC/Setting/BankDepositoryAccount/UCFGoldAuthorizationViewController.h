e491bdb9667029c6051caa48c86217c1076ae5b9
hanqiyuan <3152950791@qq.com>
2017-07-12 22:32:51 +0800

    提交配置文件

---
+++ JRGC/Setting/BankDepositoryAccount/UCFGoldAuthorizationViewController.h
@@ -0,0 +1,13 @@
+//
+//  UCFGoldAuthorizationViewController.h
+//  JRGC
+//
+//  Created by hanqiyuan on 2017/7/10.
+//  Copyright © 2017年 qinwei. All rights reserved.
+//
+
+#import "UCFBaseViewController.h"
+
+@interface UCFGoldAuthorizationViewController : UCFBaseViewController
+
+@end
