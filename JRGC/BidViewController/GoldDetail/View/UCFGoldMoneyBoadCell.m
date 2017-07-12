0183bf5e17cb6ae8b278cdef087e9c0f137d3354
hanqiyuan <3152950791@qq.com>
2017-07-12 11:12:04 +0800

    黄金详情-黄金投资页面

---
+++ JRGC/BidViewController/GoldDetail/View/UCFGoldMoneyBoadCell.m
@@ -0,0 +1,24 @@
+//
+//  UCFGoldMoneyBoadCell.m
+//  JRGC
+//
+//  Created by hanqiyuan on 2017/7/11.
+//  Copyright © 2017年 qinwei. All rights reserved.
+//
+
+#import "UCFGoldMoneyBoadCell.h"
+
+@implementation UCFGoldMoneyBoadCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
