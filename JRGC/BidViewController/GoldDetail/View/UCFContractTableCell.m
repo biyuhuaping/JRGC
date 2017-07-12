a110cb54d652d1be20cee0bcc78dbf272899289a
hanqiyuan <3152950791@qq.com>
2017-07-12 11:44:54 +0800

    添加黄金授权页面

---
+++ JRGC/BidViewController/GoldDetail/View/UCFContractTableCell.m
@@ -0,0 +1,30 @@
+//
+//  UCFContractTableCell.m
+//  JRGC
+//
+//  Created by hanqiyuan on 2017/7/10.
+//  Copyright © 2017年 qinwei. All rights reserved.
+//
+
+#import "UCFContractTableCell.h"
+
+@interface UCFContractTableCell()
+
+
+
+@end
+
+@implementation UCFContractTableCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
