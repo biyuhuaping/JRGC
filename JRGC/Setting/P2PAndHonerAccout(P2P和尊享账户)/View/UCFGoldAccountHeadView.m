dd34c042d80c36bb9cefd49b74378f8fb4239256
张瑞超 <359599907@qq.com>
2017-07-13 13:22:11 +0800

    上传事实黄金接口

--- JRGC/Setting/P2PAndHonerAccout(P2P和尊享账户)/View/UCFGoldAccountHeadView.m
+++ JRGC/Setting/P2PAndHonerAccout(P2P和尊享账户)/View/UCFGoldAccountHeadView.m
@@ -11,6 +11,8 @@
 #import "YcArray.h"
 #import "YcMutableArray.h"
 #import "UIDic+Safe.h"
+#import "ToolSingleTon.h"
+
 @interface UCFGoldAccountHeadView ()
 @property (weak, nonatomic) IBOutlet UILabel *holdGoldGram;
 
@@ -24,6 +26,7 @@
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *floatGoldSpace;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *dealGoldSpace;
 @property (weak, nonatomic) IBOutlet UIView *upBaseView;
+@property (weak, nonatomic) IBOutlet UIButton *updateGoldPriceBtn;
 @end
 
 @implementation UCFGoldAccountHeadView
@@ -33,6 +36,10 @@
     [alertView show];
 }
 - (IBAction)currentTimePriceBtnClicked:(UIButton *)sender {
+     [[ToolSingleTon sharedManager] getGoldPrice];
+     [ToolSingleTon sharedManager].currentPrice = ^(double price){
+    
+     };
 }
 - (IBAction)recoverBtnClicked:(UIButton *)sender {
     MjAlertView *alertView = [[MjAlertView alloc] initGoldAlertTitle:@"总待收黄金" Message:@"总待收黄金=已购黄金+到期黄金" delegate:self];
@@ -54,11 +61,35 @@
 }
 - (void)updateGoldAccount:(NSDictionary *)dataDic
 {
-    self.holdGoldGram.text = [dataDic objectSafeForKey:@"holdGoldAmount"];
-    self.availableGoldNum.text = [dataDic objectSafeForKey:@"availableGoldAmount"];
-    self.totalRecoveryGold.text = [dataDic objectSafeForKey:@"collectGoldAmount"];
-    self.dealGoldPrice.text = [dataDic objectSafeForKey:@"dealPrice"];
-    
+    self.holdGoldGram.text =[NSString stringWithFormat:@"%@克",[dataDic objectSafeForKey:@"holdGoldAmount"]] ;
+    NSString  *goldValue = [self switchGoldPriceFormat:[dataDic objectSafeForKey:@"availableGoldAmount"]];
+    NSString *availeStr = [NSString stringWithFormat:@"%@克(当前市值约%@)",[dataDic objectSafeForKey:@"availableGoldAmount"],goldValue];
+    self.availableGoldNum.text = availeStr;
+    self.realtimeGoldPrice.text = [NSString stringWithFormat:@"%.2f",[ToolSingleTon sharedManager].readTimePrice];
+    self.totalRecoveryGold.text = [NSString stringWithFormat:@"%@克",[dataDic objectSafeForKey:@"collectGoldAmount"]];
+    self.dealGoldPrice.text =  [NSString stringWithFormat:@"￥%@",[dataDic objectSafeForKey:@"dealPrice"]];
+    double floatValue1 = ([ToolSingleTon sharedManager].readTimePrice - [[dataDic objectSafeForKey:@"dealPrice"] doubleValue]) * [[dataDic objectSafeForKey:@"holdGoldAmount"] doubleValue];
+    if (floatValue1 >= 0) {
+        self.floatLabel.textColor = UIColorWithRGB(0xfd4d4c);
+        self.floatLabel.text = [NSString stringWithFormat:@"+￥%.2f",floatValue1];
+    } else {
+        self.floatLabel.textColor = UIColorWithRGB(0x4db94f);
+        self.floatLabel.text = [NSString stringWithFormat:@"-￥%.2f",floatValue1];
+    }
+
+}
+- (NSString *)switchGoldPriceFormat:(NSString *)availableGoldAmount
+{
+    double goldValue = [availableGoldAmount doubleValue] *[ToolSingleTon sharedManager].readTimePrice;
+    if (goldValue < 9999.95) {
+        return [NSString stringWithFormat:@"%.0f",goldValue];
+    } else if (goldValue >= 9999.95 && goldValue < 999999.95) {
+        return [NSString stringWithFormat:@"%.0f万",goldValue/10000.0f];
+    } else if (goldValue >= 999999.95){
+        return [NSString stringWithFormat:@"%.0f千万",goldValue/10000000.0f];
+    } else {
+        return 0;
+    }
 }
 
 @end
