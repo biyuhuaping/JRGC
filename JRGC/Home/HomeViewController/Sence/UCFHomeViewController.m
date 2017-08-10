a39c173ddfd0b61a8dfa8b3b45d4388796d0a8f1
njw <niujianwei@ucfgroup.com>
2017-08-10 13:42:02 +0800

    修改注册页面协议点击崩溃 修改首页图标加载不出来 修改开户后无法预约

--- JRGC/Home/HomeViewController/Sence/UCFHomeViewController.m
+++ JRGC/Home/HomeViewController/Sence/UCFHomeViewController.m
@@ -111,7 +111,7 @@
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshUI:) name:@"getPersonalCenterNetData" object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(responds3DTouchClick) name:@"responds3DTouchClick" object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(setDefaultState:) name:@"setDefaultViewData" object:nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fetchData) name:@"refreshUserState" object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshUI:) name:@"refreshUserState" object:nil];
     }
     return self;
 }
