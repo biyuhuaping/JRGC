d5365c03c56c8ed3916124ed82a7360087a13e23
njw <niujianwei@ucfgroup.com>
2017-08-15 18:41:40 +0800

    添加黄金变现界面

--- JRGC/Home/GoldTurnToMoney(黄金变现)/Controller/UCFGoldCashViewController.m
+++ JRGC/Home/GoldTurnToMoney(黄金变现)/Controller/UCFGoldCashViewController.m
@@ -7,6 +7,12 @@
 //
 
 #import "UCFGoldCashViewController.h"
+#import "UCFGoldCashFirstCell.h"
+#import "UCFGoldCashSecondCell.h"
+#import "UCFGoldCashThirdCell.h"
+#import "UCFGoldCashFourthCell.h"
+#import "UCFGoldCashButtonCell.h"
+#import "UCFGoldCashTipCell.h"
 
 @interface UCFGoldCashViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableview;
@@ -24,17 +30,78 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 0;
+    return 6;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 0;
+    if (section == 5) {
+        return 2;
+    }
+    return 1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    static NSString *cellId = @"goldcashfirst";
+    if (indexPath.section == 0) {
+        UCFGoldCashFirstCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+        if (nil == cell) {
+            cell = (UCFGoldCashFirstCell *)[[[NSBundle mainBundle] loadNibNamed:@"UCFGoldCashFirstCell" owner:self options:nil] lastObject];
+        }
+        return cell;
+    }
+    else if (indexPath.section == 1) {
+        cellId = @"goldcashsecond";
+        UCFGoldCashSecondCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+        if (nil == cell) {
+            cell = (UCFGoldCashSecondCell *)[[[NSBundle mainBundle] loadNibNamed:@"UCFGoldCashSecondCell" owner:self options:nil] lastObject];
+        }
+        return cell;
+    }
+    else if (indexPath.section == 2) {
+        cellId = @"goldcashthird";
+        UCFGoldCashThirdCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+        if (nil == cell) {
+            cell = (UCFGoldCashThirdCell *)[[[NSBundle mainBundle] loadNibNamed:@"UCFGoldCashThirdCell" owner:self options:nil] lastObject];
+        }
+        return cell;
+    }
+    else if (indexPath.section == 3) {
+        cellId = @"goldcashforth";
+        UCFGoldCashFourthCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+        if (nil == cell) {
+            cell = (UCFGoldCashFourthCell *)[[[NSBundle mainBundle] loadNibNamed:@"UCFGoldCashFourthCell" owner:self options:nil] lastObject];
+        }
+        return cell;
+    }
+    else if (indexPath.section == 4) {
+        cellId = @"goldcashbutton";
+        UCFGoldCashButtonCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+        if (nil == cell) {
+            cell = (UCFGoldCashButtonCell *)[[[NSBundle mainBundle] loadNibNamed:@"UCFGoldCashButtonCell" owner:self options:nil] lastObject];
+        }
+        return cell;
+    }
+    else if (indexPath.section == 5) {
+        cellId = @"goldcashtip";
+        UCFGoldCashTipCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+        if (nil == cell) {
+            cell = (UCFGoldCashTipCell *)[[[NSBundle mainBundle] loadNibNamed:@"UCFGoldCashTipCell" owner:self options:nil] lastObject];
+        }
+        return cell;
+    }
     return nil;
 }
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 0) {
+        return 130;
+    }
+    else
+        return 44;
+}
+
 
 @end
