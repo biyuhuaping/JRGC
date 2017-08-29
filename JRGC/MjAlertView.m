e1666726b331f8cf8566031a1b1d3828f22bfaa8
张瑞超 <359599907@qq.com>
2017-08-29 17:34:44 +0800

    更改文案

--- JRGC/MjAlertView.m
+++ JRGC/MjAlertView.m
@@ -400,8 +400,8 @@
         UILabel *msgLab = [baseView viewWithTag:202];
         if (type == MjGoldAlertViewTypeFloat) {
             titleLab.text = @"累计盈亏";
-            midLab.text = @"累计盈亏指消费者在金融工场尊享金的累计盈利或亏损";
-            msgLab.text = @"消费者总盈亏=(消费者持有黄金市值+消费者变现总金额+消费者提取金条市值)-消费者购买总金额 ";
+            midLab.text = @"累计盈亏指消费者在尊享金的累计盈利或亏损";
+            msgLab.text = @"消费者总盈亏=(消费者持有黄金市值+消费者变现总金额+消费者提取金条市值+活期累计收益)-消费者实际购买总金额";
         } else if (type == MjGoldAlertViewTypeAverage) {
             titleLab.text = @"买入均价";
             midLab.text= @"买入均价指消费者购买黄金的加权平均价";
