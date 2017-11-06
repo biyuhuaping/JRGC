ef44aa7f198d05932af30dc21291330b36dc5ba0
hanqiyuan <3152950791@qq.com>
2017-11-06 14:59:52 +0800

    提金页面UI

---
+++ JRGC/Setting/ExtractGold(提金)/View/UCFExtractViewCell.h
@@ -0,0 +1,13 @@
+//
+//  UCFExtractViewCell.h
+//  JRGC
+//
+//  Created by hanqiyuan on 2017/11/6.
+//  Copyright © 2017年 JRGC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UCFExtractViewCell : UITableViewCell
+
+@end
