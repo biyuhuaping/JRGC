c3598d77d2ad63a1eb5cc9ab5263fa76d5acf657
zhangruichao <zhangruichao@ucfgroup.com>
2018-08-10 15:13:54 +0800

    修复正常情况下，轮播图刷不出来的原因

--- JRGC/Aplication/AppDelegate.mm
+++ JRGC/Aplication/AppDelegate.mm
@@ -706,11 +706,13 @@
                         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.05 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                             [self checkNovicePoliceOnOff];
                         });
-                        [[NSNotificationCenter defaultCenter] postNotificationName:@"RefreshCycleImage" object:nil];
                     }
                 }
-//                return;
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"RefreshCycleImage" object:nil];
+
             } else {
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"RefreshCycleImage" object:nil];
+
                 NSString *des = dic[@"updateInfo"];
                 if (versionMark == 0) {
                     if (_isComeForceUpdate) {
