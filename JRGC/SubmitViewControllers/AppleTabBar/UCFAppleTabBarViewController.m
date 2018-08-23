d3e5b07f060071331179d7b73395168544c2fe58
zhangruichao <zhangruichao@ucfgroup.com>
2018-08-23 14:55:01 +0800

    修改tabbar图片

--- JRGC/SubmitViewControllers/AppleTabBar/UCFAppleTabBarViewController.m
+++ JRGC/SubmitViewControllers/AppleTabBar/UCFAppleTabBarViewController.m
@@ -45,12 +45,12 @@
                                   @"我的"];
     
     NSArray *tabbarNormalArray = @[@"tabbar_icon_homepage_normal",
-                                   @"tabbar_icon_find_normal",
+                                   @"tabbar_icon_class_normal",
                                    @"tabbar_icon_life_normal",
                                    @"tabbar_icon_user_normal"];
     
     NSArray *tabbarHighlightArray = @[@"tabbar_icon_homepage_highlight",
-                                      @"tabbar_icon_find_highlight",
+                                      @"tabbar_icon_class_highlight",
                                       @"tabbar_icon_life_highlight",
                                       @"tabbar_icon_user_highlight"];
     UIViewController *controller = nil;
