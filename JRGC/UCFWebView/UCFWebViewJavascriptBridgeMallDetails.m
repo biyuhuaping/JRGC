b42a21cd0953f0b8147d70dbbb2544cdfaeb143c
zhangruichao <zhangruichao@ucfgroup.com>
2018-10-09 16:56:53 +0800

    修复web页面双头问题

--- JRGC/UCFWebView/UCFWebViewJavascriptBridgeMallDetails.m
+++ JRGC/UCFWebView/UCFWebViewJavascriptBridgeMallDetails.m
@@ -41,7 +41,7 @@
 
 //只要是豆哥商城的都去掉导航栏
 - (void)viewWillAppear:(BOOL)animated {
-    [self.navigationController setNavigationBarHidden:!_isHidenNavigationbar animated:animated];
+    [self.navigationController setNavigationBarHidden:self.isHideNativeNav animated:animated];
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
 }
 
