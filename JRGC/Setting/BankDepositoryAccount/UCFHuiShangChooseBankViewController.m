034640e7d019ffdac8b1054c807a7b453eea4cb3
kuangzhanzhidian <kuangzhanzhidian@qq.com>
2018-11-22 12:04:55 +0800

    修改银行卡列表样式

--- JRGC/Setting/BankDepositoryAccount/UCFHuiShangChooseBankViewController.m
+++ JRGC/Setting/BankDepositoryAccount/UCFHuiShangChooseBankViewController.m
@@ -262,6 +262,17 @@
         }
     }
 }
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if ([cell respondsToSelector:@selector(setSeparatorInset:)])
+    {
+        [cell setSeparatorInset:UIEdgeInsetsMake(0, 54, 0, 15)];
+    }
+    if ([cell respondsToSelector:@selector(setLayoutMargins:)])
+    {
+        [cell setLayoutMargins:UIEdgeInsetsMake(0, 54, 0, 15)];
+    }
+}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
